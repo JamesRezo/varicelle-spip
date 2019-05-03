01cd4a96e0fe954dfb3c53fa7c4b4fafc6f5574c
tetue <tetue@rezo.net>
2019-05-03 07:29:32 +0200

    Sans oublier les autres langues :P

---
+++ lang/paquet-varicelle_en.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'comments_description' => 'Pretty buttons: seductive, light and powerful, to stylize all buttons on the site. Applies to anything with the <code>.btn</code> selector, including SPIP forms buttons.',
+	'comments_nom' => 'Varicella'
+	'comments_slogan' => 'Pretty buttons!'
+);
---
+++ lang/paquet-varicelle_es.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'comments_description' => 'Botones bonitos: seductor, ligero y potente, para estilizar todos los botones en el sitio. Se aplica a cualquier cosa con el selector <code>.btn</code> incluyendo los botones SPIP formas.',
+	'comments_nom' => 'Varicela'
+	'comments_slogan' => '¡Botones bonitos!'
+);
