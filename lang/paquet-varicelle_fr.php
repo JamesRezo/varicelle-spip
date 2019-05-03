183a9ab7a5ed9dc3f97874a3021f3e63564ac297
tetue <tetue@rezo.net>
2019-05-03 07:14:02 +0200

    Plus besoin de forcer la class .btn depuis https://zone.spip.net/trac/spip-zone/changeset/111773/spip-zone \o/

---
+++ lang/paquet-varicelle_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/varicelle/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'comments_description' => 'De jolis boutons&nbsp;: s&#233;duisants, l&#233;gers et performants, pour styler tous les boutons du site. S\'applique &#224; tout ce qui porte le s&#233;lecteur <code>.btn</code> y&nbsp;compris les boutons des formulaires&nbsp;SPIP.',
+	'comments_slogan' => 'De jolis boutons&nbsp;!'
+);
